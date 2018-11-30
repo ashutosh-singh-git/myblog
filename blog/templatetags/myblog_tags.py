ec2a87a1fbcfe7f6ee26bfa616192c9ac60660a0
Ashutosh Singh <ashutosh.singh5335@gmail.com>
2018-11-30 15:13:06 +0530

    Markdown for images

--- blog/templatetags/myblog_tags.py
+++ blog/templatetags/myblog_tags.py
@@ -12,5 +12,5 @@ def strip_double_quotes(quoted_string):
 def process_image(html):
     return html.replace('<img', '<figure class="figure text-center" style="margin:0"><img class="figure-img img-fluid w-100 rounded"') \
         .replace('/caption>', '/figcaption></figure>') \
-        .replace('<caption', '<figcaption class="figure-caption"')\
+        .replace('<caption', '<figcaption class="figure-caption"') \
         .replace('<p></p>','')
